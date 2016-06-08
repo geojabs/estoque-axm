3bc60d6da5f46327ba36ee2f49d6316a7aa9c21b
George Bentes <george.bentes@gmail.com>
2016-06-08 17:17:45 -0400

    I changed the initial route to redirect the user to another page.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	res.render('login', { title: 'AXM - Fluxo de Estoque de Produtos' });
+});
+
+router.get('/sistema', function(req, res, next) {
 	res.render('index', { title: 'AXM - Fluxo de Estoque de Produtos' });
 });
 
